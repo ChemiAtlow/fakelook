dab485530fb4a32581ed1520cf6dc895e8332e99
Chemi Atlow <chemiatlow@gmail.com>
2021-02-16 22:03:49 +0200

    stuff

---
+++ packages/common/src/constants/urls.ts
@@ -0,0 +1,6 @@
+export const URLS = {
+    serverPort: process.env.PORT ?? 4000,
+    clientPort: process.env.CLIENT_PORT ?? 3000,
+    serverDomain: process.env.SERVER_DOMAIN ?? "http://localhost",
+    clientDomain: process.env.CLIENT_DOMAIN ?? "http://localhost",
+};
