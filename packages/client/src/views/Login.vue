2fac9c2435bde7de18b283e61a5402a91e8212c8
Ariel <arielerezsarphatie@gmail.com>
2021-02-17 23:54:29 +0200

    remove timeout and add password

--- packages/client/src/views/Login.vue
+++ packages/client/src/views/Login.vue
@@ -1,34 +1,27 @@
 <template>
-    <Container>
-        <h1>Login</h1>
-        <FormField label="Username" v-model="username" />
-        <FormField label="Password" v-model="username" />
-    </Container>
+  <Container>
+    <h1>Login</h1>
+    <FormField label="Username" v-model="username" />
+    <FormField label="Password" v-model="password" type="password" />
+  </Container>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { Container } from "@/components/Layout";
 import { FormField } from "@/components/Forms";
-async function fetchData() {
-    return new Promise<void>(resolve => {
-        setTimeout(resolve, 2000);
-    });
-}
+
 const component = defineComponent({
-    name: "Login",
-    components: { Container, FormField },
-    async setup() {
-        const username = ref("");
-        console.log("start fetch!");
-        await fetchData();
-        console.log("done");
-        return { username };
-    }
+  name: "Login",
+  components: { Container, FormField },
+  async setup() {
+    const username = ref("");
+    const password = ref("");
+    return { username, password };
+  },
 });
 
 export default component;
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
