fbb3bd5ac440678ecbd929dfcef3a0ea3a644ae9
Ariel <arielerezsarphatie@gmail.com>
2021-02-17 23:20:03 +0200

    use guid

--- packages/server/src/middleware/reqId.middleware.ts
+++ packages/server/src/middleware/reqId.middleware.ts
@@ -1,8 +1,9 @@
+import { models } from "@fakelook/common";
 import { NextFunction, Request, Response } from "express";
 import { appLoggerService } from "../services";
 
 export function assignId(req: Request, _: Response, next: NextFunction) {
-    req.id = "TEMP";
+    req.id = models.classes.Guid.new();
     if (req.method !== "OPTIONS") {
         appLoggerService.verbose(`${req.method} - Req ${req.id} has begun to: ${req.path}.`);
     }
