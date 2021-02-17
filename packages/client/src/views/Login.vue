5509b2d1efd7c9a8980dddbefd823f1635c07861
Chemi Atlow <chemiatlow@gmail.com>
2021-02-18 01:05:50 +0200

    work on ui form

--- packages/client/src/views/Login.vue
+++ packages/client/src/views/Login.vue
@@ -1,24 +1,37 @@
 <template>
-  <Container>
-    <h1>Login</h1>
-    <FormField label="Username" v-model="username" />
-    <FormField label="Password" v-model="password" type="password" />
-  </Container>
+    <Container>
+        <form>
+            <h1>Login</h1>
+            <FormField label="Username" v-model="username" />
+            <FormField label="Password" v-model="password" type="password" />
+            <Button>Login</Button>
+        </form>
+        <div>
+            <Button varaiety="secondary">Forgot password?</Button>
+            <Button varaiety="secondary">Don't have an account?</Button>
+        </div>
+        <div>
+            <Button color="gray" icon="facebook">Login with Facebook</Button>
+        </div>
+        <div>
+            <Button color="gray" icon="google">Login with Google</Button>
+        </div>
+    </Container>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { Container } from "@/components/Layout";
-import { FormField } from "@/components/Forms";
+import { FormField, Button } from "@/components/Forms";
 
 const component = defineComponent({
-  name: "Login",
-  components: { Container, FormField },
-  async setup() {
-    const username = ref("");
-    const password = ref("");
-    return { username, password };
-  },
+    name: "Login",
+    components: { Container, FormField, Button },
+    async setup() {
+        const username = ref("");
+        const password = ref("");
+        return { username, password };
+    }
 });
 
 export default component;
