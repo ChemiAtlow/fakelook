dc53ca600921111f62a58cd117fb3327561abd68
Chemi Atlow <chemiatlow@gmail.com>
2021-02-18 09:17:43 +0200

    Export colors as variables

--- packages/client/src/shims-vue.d.ts
+++ packages/client/src/shims-vue.d.ts
@@ -1,6 +1,32 @@
 /* eslint-disable */
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+    import type { DefineComponent } from "vue";
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
+
+declare module "@/utils/_exportVars.scss" {
+    type vars = {
+        anotherGray: string,
+        dark: string,
+        darkBlue: string,
+        darkGray: string,
+        darkGreen: string,
+        darkRed: string,
+        darkerBlue: string,
+        deepBlue: string,
+        gray: string,
+        grayishBlue: string,
+        light: string,
+        lightGray: string,
+        lightGreen: string,
+        lightRed: string,
+        midGray: string,
+        midLightGray: string,
+        primary: string,
+        red: string,
+        secondary: string,
+    }
+    const def: vars;
+    export default def;
 }
