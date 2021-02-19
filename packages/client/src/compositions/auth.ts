836464c081c154b517ae1afcc53d22da265bd8b3
Ariel <arielerezsarphatie@gmail.com>
2021-02-19 14:15:43 +0200

    add validations

--- packages/client/src/compositions/auth.ts
+++ packages/client/src/compositions/auth.ts
@@ -1,27 +1,73 @@
-import { computed, reactive } from "vue";
+import { computed, reactive, watch } from "vue";
+import { constants } from "@fakelook/common";
 import router from "@/router";
 
 const { currentRoute } = router;
+const { validators } = constants;
 
 export const username = reactive({ value: "", error: "" });
 export const password = reactive({ value: "", error: "" });
+export const repeatPassword = reactive({ value: "", error: "" });
+export const email = reactive({ value: "", error: "" });
 
 export const isLogin = computed(() => /login/.test(currentRoute.value.path));
 export const isSignup = computed(() => /signup/.test(currentRoute.value.path));
-export const isRecover = computed(() => /recover/.test(currentRoute.value.path));
+export const isRecover = computed(() =>
+  /recover/.test(currentRoute.value.path)
+);
+
+watch(
+  () => username.value,
+  (val) => {
+    if (isLogin.value) {
+      if (!validators.username.test(val) && !validators.email.test(val))
+        username.error = "This is not a valid username/email!";
+      else username.error = "";
+    } else {
+      if (!validators.username.test(val))
+        username.error = "username is not valid!";
+      else username.error = "";
+    }
+  }
+);
+
+watch(
+  () => password.value,
+  (val) => {
+    if (!validators.password.test(val))
+      password.error = "password is not valid!";
+    else password.error = "";
+  }
+);
+
+watch(
+  [() => repeatPassword.value, () => password.value],
+  ([repVal, pasVal]) => {
+    if (repVal !== pasVal) repeatPassword.error = "passwords do not match!";
+    else repeatPassword.error = "";
+  }
+);
+
+watch(
+  () => email.value,
+  (val) => {
+    if (!validators.email.test(val)) email.error = "email is not valid!";
+    else email.error = "";
+  }
+);
 
 export const pageTitle = computed(() => {
-    switch (currentRoute.value.path) {
-        case "/login":
-            return "Login";
-        case "/signup":
-            return "Signup";
-        case "/recover":
-            return "Recover forgotten password";
-        default:
-            return "Reset password";
-    }
+  switch (currentRoute.value.path) {
+    case "/login":
+      return "Login";
+    case "/signup":
+      return "Signup";
+    case "/recover":
+      return "Recover forgotten password";
+    default:
+      return "Reset password";
+  }
 });
 export const changeView = (path: "login" | "signup" | "recover") => {
-    router.replace({ path: `/${path}` });
+  router.replace({ path: `/${path}` });
 };
--- packages/client/src/views/Auth.vue
+++ packages/client/src/views/Auth.vue
@@ -1,53 +1,97 @@
 <template>
-    <Container>
-        <form>
-            <h1>{{ pageTitle }}</h1>
-            <FormField
-                label="Username"
-                v-model="username.value"
-                autocomplete="username"
-            />
-            <FormField
-                label="Password"
-                v-model="password.value"
-                autocomplete="current-password"
-                type="password"
-            />
-            <Button>Login</Button>
-        </form>
-        <div class="btn__wrapper">
-            <Button varaiety="secondary" @click="changeView('recover')" v-if="isLogin">
-                Forgot password?
-            </Button>
-            <Button varaiety="secondary" @click="() => changeView(isLogin ? 'signup' : 'login')">
-                {{isLogin ? "Don't have an account?" : "Go to login page!" }}
-            </Button>
-        </div>
-        <template v-if="isLogin">
-            <div class="btn__wrapper">
-                <Button color="gray" icon="facebook"
-                    >Login with Facebook</Button
-                >
-            </div>
-            <div class="btn__wrapper">
-                <Button color="gray" icon="google">Login with Google</Button>
-            </div>
-        </template>
-    </Container>
+  <Container>
+    <form>
+      <h1>{{ pageTitle }}</h1>
+      <FormField
+        label="Username"
+        v-model="username.value"
+        autocomplete="username"
+        :error="username.error"
+        v-if="!isRecover"
+      />
+      <FormField
+        label="Email"
+        v-model="email.value"
+        autocomplete="email"
+        :error="email.error"
+        v-if="isSignup || isRecover"
+      />
+      <FormField
+        label="Password"
+        v-model="password.value"
+        :autocomplete="isLogin ? 'current-password' : 'new-password'"
+        type="password"
+        :error="password.error"
+        v-if="!isRecover"
+      />
+      <FormField
+        v-if="isSignup"
+        label="Repeat Password"
+        v-model="repeatPassword.value"
+        type="password"
+        autocomplete="off"
+        :error="repeatPassword.error"
+      />
+      <Button>Submit</Button>
+    </form>
+    <div class="btn__wrapper">
+      <Button
+        varaiety="secondary"
+        @click="changeView('recover')"
+        v-if="isLogin"
+      >
+        Forgot password?
+      </Button>
+      <Button
+        varaiety="secondary"
+        @click="() => changeView(isLogin ? 'signup' : 'login')"
+      >
+        {{ isLogin ? "Don't have an account?" : "Go to login page!" }}
+      </Button>
+    </div>
+    <template v-if="isLogin">
+      <div class="btn__wrapper">
+        <Button color="gray" icon="facebook">Login with Facebook</Button>
+      </div>
+      <div class="btn__wrapper">
+        <Button color="gray" icon="google">Login with Google</Button>
+      </div>
+    </template>
+  </Container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Container } from "@/components/Layout";
 import { FormField, Button } from "@/components/Forms";
-import { username, password, isLogin, pageTitle, changeView } from "@/compositions/auth";
+import {
+  username,
+  password,
+  email,
+  repeatPassword,
+  isLogin,
+  isSignup,
+  isRecover,
+  pageTitle,
+  changeView,
+} from "@/compositions/auth";
 
 const component = defineComponent({
-    name: "Login",
-    components: { Container, FormField, Button },
-    setup() {
-        return { username, password, changeView, isLogin, pageTitle };
-    }
+  name: "Auth",
+  components: { Container, FormField, Button },
+  setup() {
+    return {
+      username,
+      password,
+      email,
+      repeatPassword,
+      changeView,
+      isLogin,
+      isSignup,
+      isRecover,
+      pageTitle,
+    };
+  },
 });
 
 export default component;
@@ -55,6 +99,6 @@ export default component;
 
 <style lang="scss" scoped>
 .btn__wrapper {
-    margin: 0.5rem 0;
+  margin: 0.5rem 0;
 }
 </style>
