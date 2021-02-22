761a356a0c87ec39ff0b6d63e7830abb08a974c7
Chemi Atlow <chemiatlow@gmail.com>
2021-02-22 20:47:01 +0200

    add comments

--- packages/common/src/constants/urls.ts
+++ packages/common/src/constants/urls.ts
@@ -8,10 +8,13 @@ export const URLS = {
     //Auth                                                                                                     
     authPort: process.env.AUTH_PORT ?? 4441,
     authDomain: process.env.AUTH_DOMAIN ?? "http://localhost",
+    //Identity
     identityPort: process.env.IDENTITY_PORT ?? 4443,
     identityDomain: process.env.IDENTITY_DOMAIN ?? "http://localhost",
+    //View
     viewPort: process.env.VIEW_PORT ?? 4445,
     viewDomain: process.env.VIEW_DOMAIN ?? "http://localhost",
+    //Posts
     postsPort: process.env.POSTS_ORT ?? 4447,
     postsDomain: process.env.POSTS_DOMAIN ?? "http://localhost",
 };
