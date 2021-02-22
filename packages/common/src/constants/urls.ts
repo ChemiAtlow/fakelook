b92ad764db342c4e6a01c63fe988cd31b8308019
Chemi Atlow <chemiatlow@gmail.com>
2021-02-22 20:46:08 +0200

    add all urls to env

--- packages/common/src/constants/urls.ts
+++ packages/common/src/constants/urls.ts
@@ -1,6 +1,17 @@
 export const URLS = {
-    serverPort: process.env.PORT ?? 4000,
+    //UI
     clientPort: process.env.CLIENT_PORT ?? 3000,
-    serverDomain: process.env.SERVER_DOMAIN ?? "http://localhost",
     clientDomain: process.env.CLIENT_DOMAIN ?? "http://localhost",
+    //SERVER
+    serverPort: process.env.PORT ?? 4000,
+    serverDomain: process.env.SERVER_DOMAIN ?? "http://localhost",
+    //Auth                                                                                                     
+    authPort: process.env.AUTH_PORT ?? 4441,
+    authDomain: process.env.AUTH_DOMAIN ?? "http://localhost",
+    identityPort: process.env.IDENTITY_PORT ?? 4443,
+    identityDomain: process.env.IDENTITY_DOMAIN ?? "http://localhost",
+    viewPort: process.env.VIEW_PORT ?? 4445,
+    viewDomain: process.env.VIEW_DOMAIN ?? "http://localhost",
+    postsPort: process.env.POSTS_ORT ?? 4447,
+    postsDomain: process.env.POSTS_DOMAIN ?? "http://localhost",
 };
