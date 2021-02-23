63eb41016dfa1e1e16602bd791ee410b83ee25a4
Ariel <arielerezsarphatie@gmail.com>
2021-02-24 00:44:55 +0200

    add common badRequest error

---
+++ packages/common/src/errors/BadRequestError.ts
@@ -0,0 +1,8 @@
+import { constants } from "..";
+import { HttpError } from "./HttpError";
+
+export class BadRequestError extends HttpError{
+    constructor(info: string) {
+        super(constants.HTTPStatuses.badRequest, `Bad Requset: ${info}`);
+    }
+}
