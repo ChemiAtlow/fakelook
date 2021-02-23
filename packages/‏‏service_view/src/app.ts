593e1b4120f2e047dd971c8b75f6b18c5bfcec8e
Chemi Atlow <chemiatlow@gmail.com>
2021-02-23 09:02:27 +0200

    add View micro service

---
+++ packages/‏‏service_view/src/app.ts
@@ -0,0 +1,38 @@
+import express, { Request } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+import { json } from "body-parser";
+import { appLoggerService } from "./services";
+// import { questionsRoutes, authRoutes, testRoutes, reportRoutes, examRoutes } from "./routes";
+import { constants, middleware, utils } from "@fakelook/common";
+const { requestIdAssignMiddleware, errorMiddleware, notFoundMiddleware } = middleware;
+
+morgan.token("id", function getId(req: Request) {
+    return req.id;
+});
+
+utils.patchRouterParamForAsyncHandling();
+const app = express();
+
+app.use(requestIdAssignMiddleware(appLoggerService));
+app.use(helmet());
+app.use(cors());
+app.use(
+    morgan(":id :method :url :status :response-time ms - :res[content-length]", {
+        stream: appLoggerService.logStream,
+    })
+);
+app.use(json());
+app.use(compression());
+
+// app.use("/auth", authRoutes);
+app.use("*", notFoundMiddleware(appLoggerService));
+app.use(errorMiddleware(appLoggerService));
+
+const { viewDomain, viewPort } = constants.URLS;
+
+app.listen(viewPort, () =>
+    appLoggerService.debug(`Fakelook server is running at ${viewDomain}:${viewPort}`)
+);
