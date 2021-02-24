0e76f785e209a8358084d10edb104bb1300d5151
Chemi Atlow <chemiatlow@gmail.com>
2021-02-24 10:09:10 +0200

    Lint

--- packages/service_authentication/src/constants/dbInfo.ts
+++ packages/service_authentication/src/constants/dbInfo.ts
@@ -1,7 +1,7 @@
-export const DbInfo = {
+export const dbInfo = {
     mySqlUser: process.env.MYSQL_USER,
-    mySqlPassword:  process.env.MYSQL_PASS,
-    mySqlDomain:  process.env.MYSQL_DOMAIN,
+    mySqlPassword: process.env.MYSQL_PASS,
+    mySqlDomain: process.env.MYSQL_DOMAIN,
     mySqlPort: process.env.MYSQL_PORT,
     mySqlDb: process.env.MYSQL_DB_NAME
-}
+};
