3c0ee9665ad60807bc224d1d8aef4e9a908f8575
Chemi Atlow <chemiatlow@gmail.com>
2021-02-24 16:23:57 +0200

    Attempt 1

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const { HotModuleReplacementPlugin } = require("webpack");
+const { NODE_ENV = "production" } = process.env;
+const PKG_DIR = path.join(__dirname, "packages");
+const COMMON_DIR = path.join(PKG_DIR, "common");
+const SERVER_DIR = path.join(PKG_DIR, "server");
+const AUTH_DIR = path.join(PKG_DIR, "service_authentication");
+const IDENTITY_DIR = path.join(PKG_DIR, "service_identity");
+const POSTS_DIR = path.join(PKG_DIR, "service_posts");
+const VIEW_DIR = path.join(PKG_DIR, "service_view");
+
+const tsRule = pathName => ({
+    test: /\.ts$/,
+    loader: "ts-loader",
+    include: pathName,
+    options: {
+        configFile: path.join(pathName, "tsconfig.json"),
+    },
+});
+const commonSetting = {
+    devtool: "source-map",
+    mode: NODE_ENV,
+    target: "node",
+    watch: NODE_ENV === "development",
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    externals: [nodeExternals()],
+    plugins: [
+        new HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            tsRule(COMMON_DIR),
+            tsRule(SERVER_DIR),
+            tsRule(AUTH_DIR),
+            tsRule(IDENTITY_DIR),
+            tsRule(POSTS_DIR),
+            tsRule(VIEW_DIR),
+        ],
+    },
+};
+
+module.exports = [
+    {
+        entry: {
+            common: path.join(COMMON_DIR, "src", "index.ts"),
+            server: {
+                dependOn: "common",
+                import: path.join(SERVER_DIR, "src", "app.ts"),
+            },
+            service_authentication: {
+                dependOn: "common",
+                import: path.join(AUTH_DIR, "src", "app.ts"),
+            },
+            service_identity: {
+                dependOn: "common",
+                import: path.join(IDENTITY_DIR, "src", "app.ts"),
+            },
+        },
+        output: {
+            path: PKG_DIR,
+            filename: "[name]/dist/index.js",
+        },
+        ...commonSetting,
+    },
+];
