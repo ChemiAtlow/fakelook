9b7f48b0346bff21553771fc274fd394245eb6b1
Ariel <arielerezsarphatie@gmail.com>
2021-02-26 15:59:49 +0200

    add fb contoller to index

--- packages/service_authentication/src/controllers/index.ts
+++ packages/service_authentication/src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * as googleController from './google.controller';
 export * as basicAuthController from './basicAuth.controller';
+export * as facebookController from './facebook.controller';
