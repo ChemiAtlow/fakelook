3fe52c717b693c4af380909a72d7f00ddc7a4d16
Chemi Atlow <chemiatlow@gmail.com>
2021-02-26 09:28:52 +0200

    Google auth UI - again (#23)
    * Use external component, and open popup
    
    * fix & simplify
    
    * check async
    
    * Add axios to client
    
    * create an axios handler
    
    * try google auth route
    
    * typing
    
    * Use the API
    
    * lint
    
    * simplify logic
    
    * Rename googleAuth.service.ts to auth.service.ts
    
    * Update index.ts
    
    * Update Auth.vue
    
    Co-authored-by: atlowChemi <67366097+atlowChemi@users.noreply.github.com>

---
+++ packages/client/src/services/http.service.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { constants } from "@fakelook/common";
+const { serverDomain, serverPort } = constants.URLS;
+
+export const fakelookServer = axios.create({ baseURL: `${serverDomain}:${serverPort}` });
