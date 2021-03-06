74e141ee7ae1fe44037bea1b9b89a09ee3c919dc
Ariel <arielerezsarphatie@gmail.com>
2021-03-06 13:51:53 +0200

    create http service

---
+++ packages/server/src/services/http.service.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { constants } from "@fakelook/common";
+const { authDomain, authPort } = constants.URLS;
+
+export const authServer = axios.create({ baseURL: `${authDomain}:${authPort}` });
