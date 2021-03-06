76ed69c97d0a6198209fde330adb4b17bb13e073
Ariel <arielerezsarphatie@gmail.com>
2021-03-06 21:19:32 +0200

    try catch

--- packages/service_authentication/src/controllers/basicAuth.controller.ts
+++ packages/service_authentication/src/controllers/basicAuth.controller.ts
@@ -1,36 +1,64 @@
 import { appLoggerService, basicAuthService } from "../services";
 import { Request, Response } from "express";
-import { constants, models } from "@fakelook/common";
 import { params } from "../models";
+import { constants } from "@fakelook/common";
 
-export const signup = async (req: Request<never, any, models.classes.AuthUserDto>, res: Response) => {
+import {
+    AuthUserDto,
+    RequestPasswordResetDto,
+    ResetPasswordDto,
+} from "@fakelook/common/src/models/classes";
+
+export const signup = async (
+    req: Request<never, AuthUserDto>,
+    res: Response
+) => {
     appLoggerService.info(`request ${req.id} started signup`);
-    await basicAuthService.signup(req.body);
-    appLoggerService.info(`request ${req.id} signup success`);
-    res.status(constants.HTTPStatuses.created).send({
-        message: "user created successfully",
-    });
+    try {
+        await basicAuthService.signup(req.body);
+        appLoggerService.info(`request ${req.id} signup success`);
+        res.status(constants.HTTPStatuses.created).send({
+            message: "user created successfully",
+        });
+    } catch (error) {
+        appLoggerService.info(`request ${req.id} signup failed: ${error}`);
+        res.status(constants.HTTPStatuses.unprocessableEntity).send({
+            message: `user creation failed: ${error}`,
+        });
+    }
 };
 
-export const login = async (req: Request<never, any, models.classes.AuthUserDto>, res: Response) => {
+export const login = async (
+    req: Request<never, any, AuthUserDto>,
+    res: Response
+) => {
     appLoggerService.info(`request ${req.id} started login`);
-    const jwt = await basicAuthService.login(req.body);
-    appLoggerService.info(`request ${req.id} login success`);
-    res.send(jwt);
+    try {
+        const jwts = await basicAuthService.login(req.body);
+        appLoggerService.info(`request ${req.id} login success`);
+        res.status(constants.HTTPStatuses.ok).send(jwts);
+    } catch (error) {
+        appLoggerService.info(`request ${req.id} login failed: ${error}`);
+        res.status(constants.HTTPStatuses.unprocessableEntity).send(error);
+    }
 };
 
 export const requestPasswordReset = async (
-    req: Request<never, any, models.classes.RequestPasswordResetDto>,
+    req: Request<never, any, RequestPasswordResetDto>,
     res: Response
 ) => {
-    appLoggerService.info(`request ${req.id} started password reset token creation.`);
+    appLoggerService.info(
+        `request ${req.id} started password reset token creation.`
+    );
     await basicAuthService.requestPasswordReset(req.body);
-    appLoggerService.info(`request ${req.id} succeeded creating password reset token.`);
+    appLoggerService.info(
+        `request ${req.id} succeeded creating password reset token.`
+    );
     res.send({ message: "token sent to email" });
 };
 
 export const resetPassword = async (
-    req: params.RequestWithToken<any, models.classes.ResetPasswordDto>,
+    req: params.RequestWithToken<any, ResetPasswordDto>,
     res: Response
 ) => {
     appLoggerService.info(`request ${req.id} started reseting password.`);
