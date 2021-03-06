1672cf94ccf220bda17266b428d47224ed2a337c
Ariel <arielerezsarphatie@gmail.com>
2021-03-07 01:01:04 +0200

    syntax change

--- packages/server/src/controllers/auth.controller.ts
+++ packages/server/src/controllers/auth.controller.ts
@@ -6,60 +6,60 @@ export const basicLogin = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic login, rerouting to authentication server`
     );
-    const authRes = await authServer.post("/basic/login", req.body);
+    const { data } = await authServer.post("/basic/login", req.body);
     appLoggerService.verbose(
-        `request ${req.id} finished basic login, got response from auth server: ${authRes}`
+        `request ${req.id} finished basic login, got response from auth server: ${data}`
     );
-    res.send(authRes);
+    res.send(data);
 };
 export const basicSignup = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic signup, rerouting to authentication server`
     );
-    const authRes = await authServer.post("/basic/signup", req);
+    const { data } = await authServer.post("/basic/signup", req);
     appLoggerService.verbose(
-        `request ${req.id} finished basic signup, got response from auth server: ${authRes}`
+        `request ${req.id} finished basic signup, got response from auth server: ${data}`
     );
-    res.send(authRes);
+    res.send(data);
 };
 export const basicReqReset = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic reset request, rerouting to authentication server`
     );
-    const authRes = await authServer.post("/basic/reset", req);
+    const { data } = await authServer.post("/basic/reset", req);
     appLoggerService.verbose(
-        `request ${req.id} finished basic reset request, got response from auth server: ${authRes}`
+        `request ${req.id} finished basic reset request, got response from auth server: ${data}`
     );
-    res.send(authRes);
+    res.send(data);
 };
 export const basicReset = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic reset, rerouting to authentication server`
     );
-    const authRes = await authServer.post("/basic/reset/:token", req);
+    const { data } = await authServer.post("/basic/reset/:token", req);
     appLoggerService.verbose(
-        `request ${req.id} finished basic reset, got response from auth server: ${authRes}`
+        `request ${req.id} finished basic reset, got response from auth server: ${data}`
     );
-    res.send(authRes);
+    res.send(data);
 };
 
 export const googleLogin = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started google login, rerouting to authentication server`
     );
-    const authRes = await authServer.post("/google/login", req);
+    const { data } = await authServer.post("/google/login", req);
     appLoggerService.verbose(
-        `request ${req.id} finished google login, got response from auth server: ${authRes}`
+        `request ${req.id} finished google login, got response from auth server: ${data}`
     );
-    res.send(authRes);
+    res.send(data);
 };
 export const facebookLogin = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started facebook login, rerouting to authentication server`
     );
-    const authRes = await authServer.post("/facebook/login", req);
+    const { data } = await authServer.post("/facebook/login", req);
     appLoggerService.verbose(
-        `request ${req.id} finished facebook login, got response from auth server: ${authRes}`
+        `request ${req.id} finished facebook login, got response from auth server: ${data}`
     );
-    res.send(authRes);
+    res.send(data);
 };
