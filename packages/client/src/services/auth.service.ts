3ace0f3ee24529e39afa88f5050216ce0fb40188
Ariel <arielerezsarphatie@gmail.com>
2021-03-12 16:21:14 +0200

    refresh the refresh

--- packages/client/src/services/auth.service.ts
+++ packages/client/src/services/auth.service.ts
@@ -3,15 +3,25 @@ import { fakelookServer } from "./http.service";
 
 const authRoute = "/auth/";
 
+export async function refresh() {
+    const token = window.localStorage.getItem("fakelookJWT");
+    const newToken = await fakelookServer.post("/token/refresh", token);
+    localStorage.setItem("fakelookJWT", JSON.stringify({ jwt: newToken }));
+}
+
 export async function thirdPartyConnect(
     code: string,
     origin: string,
     thirdParty: "google" | "facebook"
 ) {
-    const { data } = await fakelookServer.post<{ jwt: string }>(`${authRoute}${thirdParty}/login`, {
-        code,
-        origin,
-    });
+    const { data } = await fakelookServer.post<{ jwt: string }>(
+        `${authRoute}${thirdParty}/login`,
+        {
+            code,
+            origin,
+        }
+    );
+    console.log("thirdParty", data);
     return data;
 }
 
@@ -22,19 +32,28 @@ export async function signup(user: models.classes.AuthUserDto) {
 }
 
 export async function login(user: models.classes.AuthUserDto) {
-    const { data } = await fakelookServer.post<{ jwt: string }>(`${authRoute}basic/login`, {
-        ...user,
-    });
+    const { data } = await fakelookServer.post<{ jwt: string }>(
+        `${authRoute}basic/login`,
+        {
+            ...user,
+        }
+    );
+    console.log("login",data);
     return data;
 }
 
-export async function requestPasswordReset(user: models.classes.RequestPasswordResetDto) {
+export async function requestPasswordReset(
+    user: models.classes.RequestPasswordResetDto
+) {
     await fakelookServer.post<void>(`${authRoute}basic/reset`, {
         ...user,
     });
 }
 
-export async function resetPassword(token: string, user: models.classes.ResetPasswordDto) {
+export async function resetPassword(
+    token: string,
+    user: models.classes.ResetPasswordDto
+) {
     await fakelookServer.post<void>(`${authRoute}basic/reset/${token}`, {
         ...user,
     });
