0f3246f1c787aa68841b75875d3411bab98c18b7
Ariel <arielerezsarphatie@gmail.com>
2021-03-12 15:48:58 +0200

    jwt service improve

--- packages/service_authentication/src/services/jwt.service.ts
+++ packages/service_authentication/src/services/jwt.service.ts
@@ -4,29 +4,37 @@ import { appLoggerService } from ".";
 import { general } from "../constants";
 import { JWTInvalidError } from "../errors";
 
-export const createAccessToken = (refreshToken: string): string => {
-    const valid = verifyToken(refreshToken);
+export const genrateJWT = (data: any, expiresIn: string | number) => {
+    const jwt = sign(data, general.jwtSecret, { expiresIn });
+    appLoggerService.verbose("generated jwt", { jwt });
+    return jwt;
+};
+
+export const createAccessToken = (refreshToken: {
+    refreshToken: string;
+}): string => {
+    const valid = verifyToken(refreshToken.refreshToken);
     if (valid) {
-        appLoggerService.verbose("generate an access JWT token:");
-        return sign(refreshToken, general.jwtSecret, { expiresIn: "15m" });
+        appLoggerService.verbose("generate an access JWT token");
+        const jwt = genrateJWT(refreshToken, "2m");
+        return jwt;
     } else {
         appLoggerService.verbose("failed to decode refresh token");
-        throw new JWTInvalidError(refreshToken);
+        throw new JWTInvalidError(refreshToken.refreshToken);
     }
 };
 
 export const createRefreshToken = (user: AuthUser): string => {
     const userInfo = { ...user, password: undefined };
-    appLoggerService.verbose("generate a refresh JWT token for user", {
-        userInfo,
-    });
-    return sign(userInfo, general.jwtSecret, { expiresIn: "5h" });
+    appLoggerService.verbose("generate a refresh JWT token for user");
+    return genrateJWT(userInfo, "15m");
 };
 
 export const verifyToken = (token: string): boolean => {
     try {
-        const decoded = verify(token, general.jwtSecret);
-        appLoggerService.verbose("decoded token successfuly:", {
+        const decoded = decodeToken(token);
+        if (!decoded) return false;
+        appLoggerService.verbose(`verified token successfuly: ${token}`, {
             decoded,
         });
         return true;
@@ -36,4 +44,23 @@ export const verifyToken = (token: string): boolean => {
         });
         return false;
     }
+};
+
+export const decodeToken = (token: string): string | object => {
+    let data: string | object = "";
+    try {
+        const decoded = verify(token, general.jwtSecret);
+        if (!decoded) data = {};
+        appLoggerService.verbose(`decoded token successfuly: ${token}`, {
+            decoded,
+        });
+        data = decoded;
+    } catch (error) {
+        appLoggerService.verbose("error at decoding token:", {
+            error,
+        });
+        data = error;
+        throw new Error(error);
+    }
+    return data;
 };
