d876bf4e482fcfeb0a88beeec23e3993ac86adad
Ariel <arielerezsarphatie@gmail.com>
2021-03-12 16:20:46 +0200

    add credentials to axios instance

--- packages/client/src/services/http.service.ts
+++ packages/client/src/services/http.service.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 import { constants } from "@fakelook/common";
 const { serverDomain, serverPort } = constants.URLS;
 
-export const fakelookServer = axios.create({ baseURL: `${serverDomain}:${serverPort}` });
+export const fakelookServer = axios.create({
+    baseURL: `${serverDomain}:${serverPort}`,
+    withCredentials: true,
+});
