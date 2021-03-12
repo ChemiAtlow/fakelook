8686e4cdd6ebce20f195f89a0579b1ad3f33138c
Chemi Atlow <chemiatlow@gmail.com>
2021-03-12 15:51:51 +0200

    Another attempt for ui

--- packages/client/src/services/http.service.ts
+++ packages/client/src/services/http.service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { constants } from "@fakelook/common";
+const { VUE_APP_SERVER } = process.env;
 const { serverDomain, serverPort } = constants.URLS;
 
-export const fakelookServer = axios.create({ baseURL: `${serverDomain}:${serverPort}` });
+export const fakelookServer = axios.create({ baseURL: VUE_APP_SERVER ?? `${serverDomain}:${serverPort}` });
