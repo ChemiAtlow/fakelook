9c40dea3fbae007f437635e8b4a4a220850b0596
Chemi Atlow <chemiatlow@gmail.com>
2021-03-14 01:13:06 +0200

    fireworks

--- packages/client/entrypoint.sh
+++ packages/client/entrypoint.sh
@@ -2,7 +2,8 @@
 
 ROOT_DIR=/app
 
-echo "Replacing env constants in JS"
+echo "Replacing env constants in JS:"
+echo "Replacing 'VUE_APP_SERVER' with '${VUE_APP_SERVER}'"
 
 for file in $ROOT_DIR/js/*.*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
 do
--- packages/client/src/services/http.service.ts
+++ packages/client/src/services/http.service.ts
@@ -3,6 +3,7 @@ import { constants } from "@fakelook/common";
 const { serverDomain, serverPort } = constants.URLS;
 
 const SERVER_URL = "VUE_APP_SERVER";
+console.log("FIREWORKS!");
 
 const baseURL = SERVER_URL.startsWith("VUE_APP_") ? `${serverDomain}:${serverPort}` : SERVER_URL;
 
