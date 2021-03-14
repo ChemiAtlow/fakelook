d4ae90dbd9a83219d79ca3f3c426a48849aff61e
atlowChemi <67366097+atlowChemi@users.noreply.github.com>
2021-03-14 23:45:12 +0200

    Fix no request body

--- packages/server/src/controllers/auth.controller.ts
+++ packages/server/src/controllers/auth.controller.ts
@@ -16,7 +16,7 @@ export const basicSignup = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic signup, rerouting to authentication server`
     );
-    const { data } = await authServer.post("/basic/signup", req);
+    const { data } = await authServer.post("/basic/signup", req.body);
     appLoggerService.verbose(
         `request ${req.id} finished basic signup, got response from auth server: ${data}`
     );
@@ -26,7 +26,7 @@ export const basicReqReset = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic reset request, rerouting to authentication server`
     );
-    const { data } = await authServer.post("/basic/reset", req);
+    const { data } = await authServer.post("/basic/reset", req.body);
     appLoggerService.verbose(
         `request ${req.id} finished basic reset request, got response from auth server: ${data}`
     );
@@ -36,7 +36,7 @@ export const basicReset = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started basic reset, rerouting to authentication server`
     );
-    const { data } = await authServer.post("/basic/reset/:token", req);
+    const { data } = await authServer.post("/basic/reset/:token", req.body);
     appLoggerService.verbose(
         `request ${req.id} finished basic reset, got response from auth server: ${data}`
     );
@@ -47,7 +47,7 @@ export const googleLogin = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started google login, rerouting to authentication server`
     );
-    const { data } = await authServer.post("/google/login", req);
+    const { data } = await authServer.post("/google/login", req.body);
     appLoggerService.verbose(
         `request ${req.id} finished google login, got response from auth server: ${data}`
     );
@@ -57,7 +57,7 @@ export const facebookLogin = async (req: Request, res: Response) => {
     appLoggerService.verbose(
         `request ${req.id} started facebook login, rerouting to authentication server`
     );
-    const { data } = await authServer.post("/facebook/login", req);
+    const { data } = await authServer.post("/facebook/login", req.body);
     appLoggerService.verbose(
         `request ${req.id} finished facebook login, got response from auth server: ${data}`
     );
